e611cf6a1d3c6366335754312c5bdae73162d029
elzippre <elzippre@github.com>
2023-10-11 00:02:12 +0200

    new version with homer or organizr

--- create.sh
+++ create.sh
@@ -25,6 +25,6 @@ cp .env ./sonarr
 cp .env ./transmission
 echo "Done, copying .env files to container folders!"
 
-docker-compose -f ./jackett/docker-compose.yaml -f ./jellyfin/docker-compose.yaml -f ./lidarr/docker-compose.yaml -f ./radarr/docker-compose.yaml -f ./sonarr/docker-compose.yaml -f ./transmission/docker-compose.yaml up -d
+docker-compose -f ./prowlarr/docker-compose.yaml -f ./jellyfin/docker-compose.yaml -f ./lidarr/docker-compose.yaml -f ./radarr/docker-compose.yaml -f ./sonarr/docker-compose.yaml -f ./transmission/docker-compose.yaml up -d
 
 
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # MediaCenter Stop Script
 
-docker-compose -f ./jackett/docker-compose.yaml -f ./jellyfin/docker-compose.yaml -f ./lidarr/docker-compose.yaml -f ./radarr/docker-compose.yaml -f ./sonarr/docker-compose.yaml -f ./transmission/docker-compose.yaml --env-file .env down
+docker-compose -f ./prowlarr/docker-compose.yaml -f ./jellyfin/docker-compose.yaml -f ./lidarr/docker-compose.yaml -f ./radarr/docker-compose.yaml -f ./sonarr/docker-compose.yaml -f ./transmission/docker-compose.yaml --env-file .env down
 
 
