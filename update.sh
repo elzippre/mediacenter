d3879ae89fa15d0f4b0077f0bc30283b4d57d267
Werner Richter <werner.richter@cr14.net>
2024-11-10 11:35:09 +0100

    env added

--- update.sh
+++ update.sh
@@ -3,12 +3,12 @@
 echo "Updating images for containers"
 for app in $(cat apps2install.txt )
 do
-   docker compose -f ./$app/docker-compose.yaml pull
+   docker compose -f ./$app/docker-compose.yaml --env-file ../.env pull
 done
 echo "Done, downloading images!"
 
 for app in $(cat apps2install.txt )
 do
-   docker compose -f ./$app/docker-compose.yaml -d
+   docker compose -f ./$app/docker-compose.yaml --env-file ../.env -d
 done
 echo "Done, updating containers!"
